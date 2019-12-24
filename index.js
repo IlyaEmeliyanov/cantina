47c7a418b7aeec9612efdae2720828939735448b
KarlMarx <ergnord@gmail.com>
2019-12-24 15:43:45 +0100

    demo version3

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const info = clc.cyanBright;
 const error = clc.redBright;
 
 
-const {port, DATABASE, NODE_ENV} = require('./config/config.json');
+const {port, DATABASE_DEV, DATABASE_PROD, NODE_ENV} = require('./config/config.json');
 const serieRouter = require('./router/serieRoutes');
 const authRouter = require('./router/authRoutes');
 
@@ -20,15 +20,25 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-
 const options = {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
 };
-mongoose.connect(DATABASE, options).then(() => console.log(info('Connected to DB'))).catch(err => console.log(error('Error: ', err.message)));
+
+console.log(info(`You are in ${process.env.NODE_ENV} mode👽`));
+
+DATABASE_PROD.replace('<password>', process.env.DB_PASSWORD);
+
+if(process.env.NODE_ENV === 'production')
+    mongoose.connect(DATABASE_PROD, options).then(() => console.log(info('Connected to DB'))).catch(err => console.log(error('Error: ', err.message)));
+else
+    mongoose.connect(DATABASE_DEV, options).then(() => console.log(info('Connected to DB'))).catch(err => console.log(error('Error: ', err.message)));
+
 process.env.NODE_ENV = NODE_ENV;
+
+
 
 function config(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
@@ -39,8 +49,11 @@ function config(req, res, next) {
 
 app.use(config);
 
+app.use('/', (req, res) => {
+    res.send("Hello World");
+});
 app.use('/serie', serieRouter);
-app.use('/', authRouter);
+app.use('/main', authRouter);
 app.use('*', (req, res, next) => {
     next(new AppError(`Invalid path ${req.path}`));
 });
@@ -48,4 +61,4 @@ app.use(globalError);
 
 const connectionPort = process.env.PORT || port;
 
-app.listen(connectionPort, () => console.log(info('Connected to port: ', connectionPort)));
+app.listen(connectionPort, () => console.log(info('Connected to port:', connectionPort)));
